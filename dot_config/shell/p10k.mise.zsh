5c8fce2ad0a01c74e3014df328ba6cfbb650d05c
iamhsa <iamhsa@users.noreply.github.com>
2025-06-26 21:16:05 +0200

    Fix k9s config, mise config

--- dot_config/shell/p10k.mise.zsh
+++ dot_config/shell/p10k.mise.zsh
@@ -5,21 +5,18 @@
 
 () {
   function prompt_mise() {
-    local plugins=("${(@f)$(mise ls --current --offline 2>/dev/null | awk '!/\(symlink\)/ && $3!="~/.tool-versions" && $3!="~/.config/mise/config.toml" && $3!="(missing)" {if ($1) print $1, $2}')}")
-    local plugin
-    for plugin in ${(k)plugins}; do
-      local parts=("${(@s/ /)plugin}")
-      local tool=${(U)parts[1]}
-      if [[ $tool =~ ":" ]]; then
-        continue
-      fi
-      local version=${parts[2]}
-      p10k segment -r -i "${tool}_ICON" -s $tool -t "$version"
-    done
-  }
+   local plugins=("${(@f)$(mise ls --current --offline 2>/dev/null | awk '!/\(symlink\)/ && $3!="~/.tool-versions" && $3!="~/.config/mise/config.toml" && $3!="(missing)" {if ($1) print $1, $2}')}")
+   local plugin
+   for plugin in ${(k)plugins}; do
+     local parts=("${(@s/ /)plugin}")
+     local tool=${(U)parts[1]}
+     local version=${parts[2]}
+     p10k segment -r -i "${tool}_ICON" -s $tool -t "$version"
+   done
+ }
 
   # Colors
-  typeset -g POWERLEVEL9K_MISE_BACKGROUND=0
+  typeset -g POWERLEVEL9K_MISE_BACKGROUND=1
 
   typeset -g POWERLEVEL9K_MISE_TERRAFORM_FOREGROUND=70
   typeset -g POWERLEVEL9K_MISE_TERRAFORM_VISUAL_IDENTIFIER_EXPANSION=' '
