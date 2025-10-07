b05c6fadee299d16afdff3f8204d24d6bb4462c4
iamhsa <iamhsa@users.noreply.github.com>
2025-10-07 11:07:40 +0200

    Bump neovim config

--- dot_config/nvim/lua/plugins/copilotchat.lua
+++ dot_config/nvim/lua/plugins/copilotchat.lua
@@ -3,7 +3,8 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
     dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      -- { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "github/copilot.vim" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
     build = "make tiktoken", -- Only on MacOS or Linux
